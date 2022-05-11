a029d6bf00ec096fcf4f1652f501c59807380c63
Andy Chan <andy.chan9930@gmail.com>
2022-05-11 17:03:00 -0400

    commit tree 655064d79ce3c8b1ed2d311534369975dc4afc69

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,5 @@
 class ToysController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   wrap_parameters format: []
 
   def index
@@ -7,25 +8,34 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = find_toy
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
+
+  def render_unprocessable_entity invalid
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:name, :image, :likes, :id)
+  end
+
+  def find_toy
+    Toy.find_by(id: params[:id])
   end
 
 end
